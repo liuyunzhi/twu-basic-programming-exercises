b1c63ff89411fd6b2b2ed77359fbb2e415c9e6d4
Yunzhi Liu <liu_yun_zhi@163.com>
2018-09-05 23:30:56 +0800

    [Yunzhi LIU] feat: Draw a horizontal line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
 import triangle.Asterisk;
+import triangle.HorizontalLine;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("--------Introductory programming exercises--------");
+        System.out.println("Triangle Exercises: ");
+        System.out.println();
+        System.out.println("Easiest exercise ever: ");
         Asterisk.draw();
+        System.out.println("Draw a horizontal line: ");
+        HorizontalLine.draw(5);
     }
 }
---
+++ src/main/java/triangle/HorizontalLine.java
@@ -0,0 +1,10 @@
+package triangle;
+
+public class HorizontalLine {
+
+    public static void draw(Integer n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+}
