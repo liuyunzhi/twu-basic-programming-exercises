eb65bd8af98bac5e774ecf11f3c58bb2196ec14a
Yunzhi Liu <liu_yun_zhi@163.com>
2018-09-05 23:33:55 +0800

    [Yunzhi LIU] feat: Draw a vertical line

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import triangle.Asterisk;
 import triangle.HorizontalLine;
+import triangle.VerticalLine;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,8 @@ public class Main {
         System.out.println("Easiest exercise ever: ");
         Asterisk.draw();
         System.out.println("Draw a horizontal line: ");
-        HorizontalLine.draw(5);
+        HorizontalLine.draw(8);
+        System.out.println("Draw a vertical line: ");
+        VerticalLine.draw(3);
     }
 }
--- src/main/java/triangle/VerticalLine.java
+++ src/main/java/triangle/VerticalLine.java
@@ -1,11 +1,10 @@
 package triangle;
 
-public class HorizontalLine {
+public class VerticalLine {
 
     public static void draw(Integer n) {
         for (int i = 0; i < n; i++) {
-            System.out.print("*");
+            System.out.println("*");
         }
-        System.out.println();
     }
 }
